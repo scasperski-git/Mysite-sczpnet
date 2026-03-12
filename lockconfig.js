6fad5b85b8403cf43429315f41c259df155e8005
scasperski-git <scasperski@gmail.com>
2026-03-13 00:39:05 +0800

    Update lockconfig.js

--- lockconfig.js
+++ lockconfig.js
@@ -1,1 +1,5 @@
 const api="https://script.google.com/macros/s/AKfycbwxxWuLGQH5Tw9tJ8NP_WFTJyD8Ok4NJ4R8z4NTWeSoUsQoN8ttQQJBrllcXBoOxiYV/exec";
+/* GLOBAL SETTINGS */
+
+const SESSION_TIMEOUT = 1440;   // minutes
+const AUTO_REFRESH = 30000;     // 30 seconds
