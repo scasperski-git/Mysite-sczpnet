7084f3b24a120b44913827c141fe508206a223ac
scasperski-git <scasperski@gmail.com>
2026-03-13 00:48:14 +0800

    Update lockconfig.js

--- lockconfig.js
+++ lockconfig.js
@@ -1,4 +1,4 @@
-const api="https://script.google.com/macros/s/AKfycbwxxWuLGQH5Tw9tJ8NP_WFTJyD8Ok4NJ4R8z4NTWeSoUsQoN8ttQQJBrllcXBoOxiYV/exec";
+const API_URL="https://script.google.com/macros/s/AKfycbwxxWuLGQH5Tw9tJ8NP_WFTJyD8Ok4NJ4R8z4NTWeSoUsQoN8ttQQJBrllcXBoOxiYV/exec";
 /* GLOBAL SETTINGS */
 
 const SESSION_TIMEOUT = 1440;   // minutes
