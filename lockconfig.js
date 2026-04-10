eb0ac9d1f1d513514d02a6067d57dc8959c0800d
scasperski-git <scasperski@gmail.com>
2026-04-10 20:44:53 +0800

    Update lockconfig.js

--- lockconfig.js
+++ lockconfig.js
@@ -1,1 +1,1 @@
-const API_URL="https://script.google.com/macros/s/AKfycbyAImpjgssbhZVHLuxNQhlG_PRBQRgCMDKR-nDGImEj1JHB9x-Kv77UQfX2s5otB-VA/exec";
+const API_URL="https://script.google.com/macros/s/AKfycbyat-6NEDdFrcNb3Gcfwr57nj7-vf25GMM21Id-JJL1MUYCYOeKmOUUfNz8VGezCMiz/exec";
